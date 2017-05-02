c7e19aa69282699e1f60d5f00140584dec026d94
sirgalas <hudos@rambler.ru>
2017-05-02 16:48:04 +0300

    доделал контролеры добавил представления

---
+++ controllers/MenusetController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 02.05.17
+ * Time: 16:45
+ */
+
+namespace sirgalas\menu\controllers;
+
+
+class MenusetController
+{
+
+}
---
+++ views/menuget/_form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 02.05.17
+ * Time: 16:47
+ */
