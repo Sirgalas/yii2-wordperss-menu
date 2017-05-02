90bdce4a8bec56daff181f87361b62b394d9ad43
sirgalas <hudos@rambler.ru>
2017-05-02 16:32:21 +0300

    добавил контролер

---
+++ controllers/MenuController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 02.05.17
+ * Time: 16:31
+ */
+
+namespace sirgalas\munu\controllers;
+
+
+use yii\web\Controller;
+
+class MenuController extends Controller
+{
+
+}
