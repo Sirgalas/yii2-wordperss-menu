ae4a6e2020b58277558936771eee43a96af1df72
sirgalas <hudos@rambler.ru>
2017-05-04 15:51:04 +0300

    add style and script file

--- controllers/MenugetController.php
+++ controllers/MenugetController.php
@@ -4,11 +4,15 @@ namespace sirgalas\menu\controllers;
 
 
 use yii\web\Controller;
-
+use sirgalas\menu\models\MenuGet;
 class MenugetController extends Controller
 {
        public function actionIndex(){
            $module=$this->module;
-           return var_dump($module->getAllModels());
+           $model= new MenuGet();
+           return $this->render('create',[
+               'allModels'=>$module->getAllModels(),
+               'model'=>$model
+           ]);
        }
 }
