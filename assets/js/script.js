8d4409981c61a7192df9910fb1a6a945a7d4ce96
sirgalas <hudos@rambler.ru>
2017-05-19 12:54:59 +0300

    radact sortable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,37 @@
 jQuery(document).ready(function(){
     var startX=0;
     var count=1;
-    var depth=25;
+    var depth=50;
     var division=0;
     var maxDeptch;
     var classBootstrap;
     var prev;
-    var offset=$('#menu-to-edit').offset();
+    var offsets=$('#menu-to-edit').offset();
     $(".sortable-ui").on('click',function () {
         $(".sortable-ui").sortable({
+            grid:[50,50],
             start: function (event, ui) {
                 startX = event.clientX;
                 ui.item.removeClass(classBootstrap);
+                // console.log(ui.item.offset());
             },
-            sort: function (event, ui) {
-                var summ = event.clientX - startX;
-                //var dataDepth=ui.item.data('depth');
+            activate: function (event, ui) {
+
+            },
+            beforeStop: function(event, ui){
+                console.log(ui.offset);
+                devision = Math.round((ui.offset.left - offsets.left) / depth);
             },
             stop: function (event, ui) {
                 var classDeptch = 1;
-                startX = event.clientX;//получаю позицию движущего элимента
+                startX = event.clientX;
                 prev = ui.item.prev().context.attributes[3].nodeValue;
-                maxDeptch = parseInt(prev) + 1;//предыдущего вложенного элимента +1
-                devision = Math.round((event.clientX - offset.left) / depth);//получаю количество шагов из расчета шаг 25 пикселей
-                if (devision >= maxDeptch) {//если количество шагов больше чем глубина maxDeptch глубина вложения не может быть глубже maxDeptch
+                maxDeptch = parseInt(prev) + 1;
+                if (devision >= maxDeptch) {
                     classDeptch = maxDeptch;
                 } else {
                     classDeptch = devision;
                 }
-                console.log(ui.item.prev());
-                console.log(maxDeptch);
                 if (ui.item.index() != 0) {
                     classBootstrap = 'col-md-offset-' + classDeptch;
                     ui.item.addClass(classBootstrap);
@@ -43,3 +45,11 @@ jQuery(document).ready(function(){
         $(this).parent('.wells').remove()
     });
 });
+
+function log (evt) {
+    if (!evt) {
+        var args = '{}';
+    } else {
+        var args = evt.params;
+    }
+    return args;}
