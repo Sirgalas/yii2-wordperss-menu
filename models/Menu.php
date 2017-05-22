29c9110bb0984bbcdc4665c480437bef31d2a792
sirgalas <hudos@rambler.ru>
2017-05-22 16:16:47 +0300

    redact translit for extension

--- models/Menu.php
+++ models/Menu.php
@@ -1,17 +1,54 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 02.05.17
- * Time: 16:42
- */
+
 
 namespace sirgalas\menu\models;
 
 
 use yii\db\ActiveRecord;
-
+use sirgalas\menu\Module;
 class Menu extends ActiveRecord
 {
 
+    public static function tableName()
+    {
+        $module=self::modules;
+        if(empty($module->modelDb)){
+            return '{{%menu_table}}';
+        }else{
+            return $module->dbName;
+        }
+    }
+
+    public function rules()
+    {
+        $module=self::modules;
+        if(empty($module->modelDb)) {
+            return [
+                [['name', 'content'], 'required'],
+                [['content'], 'string'],
+                [['name'], 'string', 'max' => 510],
+                [['name'], 'unique'],
+            ];
+        }else{
+            return [
+                [[$module->name, $module->content], 'required'],
+                [[$module->content], 'string'],
+                [[$module->name,$module->serviceField], 'string', 'max' => 510],
+                [[$module->name], 'unique'],
+            ];
+        }
+    }
+    public function attributeLabels()
+    {
+        $module=self::modules;
+        $returnArray=array([
+            'id' => 'id',
+            'name' => Module::t('Name'),
+            'content'    => Module::t('Content'),
+        ]);
+        if(!empty($module->modelDb)){
+            $returnArray['serviceField']=Module::t('Service-Feild');
+        }
+        return $returnArray;
+    }
 }
--- views/menuget/index.php
+++ views/menuget/index.php
@@ -1,2 +1,30 @@
 <?php
 
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\FrontendSetupSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('backend','MENUSETUP');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="frontend-setup-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a(Yii::t('backend','CREATEMENU'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'key_setup',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
